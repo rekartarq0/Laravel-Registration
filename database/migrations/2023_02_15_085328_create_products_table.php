4a283c5d8ec44b9fc94e06ee3c04b29bbf390e4d
rekar <rekartarq49@gmail.com>
2023-02-15 12:53:25 +0300

    laravel permision role

--- database/migrations/2023_02_15_085328_create_products_table.php
+++ database/migrations/2023_02_15_085328_create_products_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer("role")->default(0);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('detail');
+            $table->string('price');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\Auth\Logincontroller;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register',[Logincontroller::class,'register']);
+Route::post('/register',[Loginontroller::class,'register']);
 Route::post('/login',[Logincontroller::class,'sigin']);
 
 Route::middleware(['auth:api'])->group(
     function(){
         Route::post('logout', [Logincontroller::class,'logout']);
+        Route::middleware(['isAdmin'])->group(
+            function(){
+                Route::get('admin/users',[ProductController::class,'index']);
+                Route::post('admin/users/create',[ProductController::class,'store']);
+            }
+        );
     }
 );
